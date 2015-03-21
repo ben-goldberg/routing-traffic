501974d9634388331c622c3110e4abd0ae965919
ben-goldberg <bgoldberg15@cmc.edu>
2015-03-21 16:57:57 -0700

    fixing bash syntax

--- nodesetup.sh
+++ nodesetup.sh
@@ -2,10 +2,10 @@
 #performs setup on a node, dropping necessary paths and setting
 #up a new default path to the first-hop router
 
-first_hop = $1
+FIRST_HOP = $1
 
 sudo route add -host 192.168.253.1 gw 192.168.1.254
 sudo route add -host 192.168.252.1 gw 192.168.1.254
 sudo route add -host 192.168.253.3 gw 192.168.1.254
 sudo route del default gw 192.168.1.254
-sudo route add default gw first_hop
+sudo route add default gw $FIRST_HOP
